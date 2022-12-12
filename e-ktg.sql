be100eafbaba5d7fbf1a2b9a60bfbc04afdde728
Bayu Pratama <106969470+bayyyyu@users.noreply.github.com>
2022-12-12 18:59:42 +0700

    Update e-ktg.sql

--- e-ktg.sql
+++ e-ktg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 07 Des 2022 pada 23.32
+-- Waktu pembuatan: 12 Des 2022 pada 11.58
 -- Versi server: 8.0.30
 -- Versi PHP: 8.1.10
 
